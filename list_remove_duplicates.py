e643939c3ef1b8529d47cda9738c12d972310c9e
Obinna <oisiwekpeni@gmail.com>
2019-05-23 00:22:43 +0200

    adding files

---
+++ list_remove_duplicates.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 22 22:58:55 2019
+
+@author: Obinna Isiwekpeni
+"""
+
+def remove_dup(new_list):
+    return set(new_list)
+print(remove_dup([1,1,2,2,4,5,6,6,7]))
+    
+
